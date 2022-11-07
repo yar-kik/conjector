0ad26a010a7b218c7e30739c270e326aedf25a29
yar-kik <einstein16.04@gmail.com>
2022-11-07 17:57:44 +0200

    Update README.md, add setup files

---
+++ setup.py
@@ -0,0 +1,25 @@
+import io
+import re
+import setuptools
+
+with io.open("version.py", encoding="utf-8") as f:
+    regex_result = re.search(r"__version__ = \"(.+)\"", f.read())
+    version = regex_result.group(1) if regex_result else "0.1.0"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-app-properties",
+    version=version,
+    author="Yaroslav Kikvadze",
+    author_email="yaroslav.k@simporter.com",
+    description="Java application properties for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yar-kik/py-app-properties",
+    license="MIT",
+    packages=["app_properties"],
+    install_requires=["PyYAML>=6.0"],
+    python_requires=">=3.8",
+)
