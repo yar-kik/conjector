45ca7a0e2e452eace6239ebc9fb0b710593d83e2
Yaroslav Kikvadze <einstein16.04@gmail.com>
2022-12-11 20:58:49 +0200

    Add lazy initialization

--- app_properties/main.py
+++ app_properties/main.py
@@ -1,5 +1,7 @@
 from typing import (
+    Any,
     Callable,
+    Dict,
     Optional,
     Type,
     TypeVar,
@@ -9,7 +11,6 @@ from typing import (
 )
 
 import functools
-import inspect
 
 from app_properties.config_handler import ConfigHandler
 from app_properties.type_converter import TypeConverter
@@ -26,6 +27,7 @@ def properties(
     override_default: bool = ...,
     root: str = ...,
     type_cast: bool = ...,
+    lazy_init: bool = ...,
 ) -> Callable[[Type[_T]], Type[_T]]:
     ...
 
@@ -39,6 +41,7 @@ def properties(
     override_default: bool = ...,
     root: str = ...,
     type_cast: bool = ...,
+    lazy_init: bool = ...,
 ) -> Type[_T]:
     ...
 
@@ -51,35 +54,59 @@ def properties(
     override_default: bool = False,
     root: str = "",
     type_cast: bool = True,
+    lazy_init: bool = False,
 ) -> Union[Callable[[Type[_T]], Type[_T]], Type[_T]]:
+    """
+    Decorator to inject config file values into class variables and cast them
+    according to type hints.
+
+    :param cls: class to inject constants. Passed implicit when decorator
+    is used with or without parenthesis.
+    :param filename: name of file with constants. `yaml` and `json` formats are
+    supported. Config file will be searched in the same directory where is file
+    with used decorator.
+    :param ignore_case: ignore case for field names or not.
+    :param override_default: override default class vars or stay as is.
+    :param root: for nested config - field names separated with dots, like
+    `some.nested.key`.
+    :param type_cast: apply type cast or stay as is.
+    :param lazy_init: inject constants immediately or lazy. If lazy - method
+    `init_props` should be called when necessary. This method also accept
+    boolean keyword param `override_init` to keep values of initialized class
+    or override them with config values.
+
+    :return: class with injected constants.
+    """
+
     @functools.wraps(cls)  # type: ignore
-    def wrapper(obj: Type[_T]) -> Type[_T]:
+    def wrapper(cls_: Type[_T]) -> Type[_T]:
+        def init_props(
+            self: Optional[_T] = None, *, override_init: bool = True
+        ) -> None:
+            # if obj is None than method was called as classmethod
+            # else we work with instance object
+            obj = cls_ if self is None else self
+            for field, value in lazy_properties.items():
+                if override_init and value:
+                    setattr(obj, field, value)
+                else:
+                    if getattr(obj, field, None) is None:
+                        setattr(obj, field, value)
+
         type_converter = TypeConverter()
-        config_handler = ConfigHandler(filename)
-        annotated_class_vars = get_type_hints(obj)
-        default_class_vars = _get_default_class_var(obj)
-        config = config_handler.get_config(ignore_case, root)
+        config = ConfigHandler(filename).get_config(ignore_case, root)
+        annotated_class_vars = get_type_hints(cls_)
+        lazy_properties: Dict[str, Any] = {}
+        setattr(cls_, "init_props", init_props)
         for class_var, type_ in annotated_class_vars.items():
-            if not override_default and class_var in default_class_vars:
-                continue
             value = config.get(class_var.lower() if ignore_case else class_var)
             if type_cast:
                 value = type_converter.cast_types(type_, value)
-            setattr(obj, class_var, value)
-        return obj
+            lazy_properties[class_var] = value
+        if not lazy_init:
+            init_props(override_init=override_default)
+        return cls_
 
     if cls is None:
         return wrapper
     return wrapper(cls)
-
-
-def _get_default_class_var(obj: type) -> dict:
-    return {
-        k: v
-        for k, v in inspect.getmembers(obj)
-        if not (
-            k.startswith("__")
-            or callable(v)
-            or isinstance(v, (property, classmethod))
-        )
-    }
---
+++ tests/test_using/test_lazy_init.py
@@ -0,0 +1,57 @@
+import pytest
+from dataclasses import dataclass
+
+from app_properties import properties
+
+
+@pytest.fixture
+def lazy_init_fixt():
+    @properties(root="lazy_init", lazy_init=True)
+    @dataclass
+    class LazyInitClass:
+        init_var: str
+        config_var: str
+        init_default_var: str = "not_exist_value"
+        config_default_var: str = "default_value"
+
+    return LazyInitClass
+
+
+def test_lazy_init_provided_only_default(lazy_init_fixt):
+    instance = lazy_init_fixt(init_var="init_var", config_var="new_value")
+    instance.init_props()
+    assert instance.init_default_var == "not_exist_value"
+
+
+def test_lazy_init_provided_only_init(lazy_init_fixt):
+    instance = lazy_init_fixt(init_var="init_var", config_var="new_value")
+    instance.init_props()
+    assert instance.init_var == "init_var"
+
+
+def test_lazy_init_provided_default_and_config(lazy_init_fixt):
+    instance = lazy_init_fixt(init_var="init_var", config_var="new_value")
+    instance.init_props()
+    assert instance.config_default_var == "another_config_value"
+
+
+def test_lazy_init_provided_all_and_override_init(lazy_init_fixt):
+    instance = lazy_init_fixt(
+        init_var="init_var",
+        config_var="new_value",
+        config_default_var="new_default_value",
+    )
+    instance.init_props()
+    assert instance.config_var == "some_config_value"
+    assert instance.config_default_var == "another_config_value"
+
+
+def test_lazy_init_provided_all_and_keep_init(lazy_init_fixt):
+    instance = lazy_init_fixt(
+        init_var="init_var",
+        config_var="new_value",
+        config_default_var="new_default_value",
+    )
+    instance.init_props(override_init=False)
+    assert instance.config_var == "new_value"
+    assert instance.config_default_var == "new_default_value"
