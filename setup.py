6bc3aa5ca0c6ee22d869c3fece63bb508eccd3ca
Yaroslav Kikvadze <einstein16.04@gmail.com>
2023-01-03 21:08:44 +0200

    Code refactoring
    Improve optional import logic;
    Update Travis-CI config;
    Update requirements.txt and linter configs.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     extras_require={
         "yaml": ["PyYAML>=6.0"],
