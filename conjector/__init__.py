c8e0699fe79573792d7d7f934da0b4a012b90b2a
y.kikvadze <y.kikvadze@smartweb.com>
2023-02-16 22:13:31 +0200

    Code refactoring

--- conjector/__init__.py
+++ conjector/__init__.py
@@ -1,4 +1,5 @@
+from .entities import Default
 from .main import Conjector, properties
 
-__all__ = ("properties", "Conjector")
+__all__ = ("properties", "Conjector", "Default")
 __version__ = "1.8.0"
